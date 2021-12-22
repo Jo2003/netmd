e6556f91de0177446b5cd15519b70a45fc6cc4d9
Jo2003 <olenka.joerg@gmail.com>
2021-12-22 11:33:24 +0100

    add header for netmdcli

---
+++ netmdcli/netmdcli.h
@@ -0,0 +1,27 @@
+/* netmdcli.h
+ *      Copyright (C) 2017 René Rebe
+ *      Copyright (C) 2002, 2003 Marc Britten
+ *
+ * This file is part of libnetmd.
+ *
+ * libnetmd is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Libnetmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+#include <unistd.h>
+#include <stdint.h>
+#include "../libnetmd/libnetmd.h"
+#include "../libnetmd/utils.h"
+
+void netmd_cli_set_json_fd(FILE* fd);
+int run_me(int argc, char* argv[]);
